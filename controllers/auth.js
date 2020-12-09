a01b1a624bc87d9a4f6632a61bdbd4f9fe2bcbb6
Paul guadalupe veliz lopez <paulsystem32@gmail.com>
2020-12-09 13:13:56 -0700

    Validando email
    Se valida si el email ya existe.

--- controllers/auth.js
+++ controllers/auth.js
@@ -2,14 +2,29 @@ const { response } = require('express');
 const Usuario = require('../models/usuario');
 
 const crearUsuario = async (req, res = response) => {
-    const usuario = new Usuario( req.body );
+    const { email } = req.body;
+    try {
+        const existeEmail = await Usuario.findOne({ email });
+        if( existeEmail ) return res.status(400).json({
+            ok: false,
+            msg: 'El correo ya esta registrado.'
+        });
+        const usuario = new Usuario( req.body );
+        await usuario.save();
 
-    await usuario.save();
+        res.json({
+            ok: true,
+            usuario
+        });
 
-    res.json({
-        ok: true,
-        msg: 'Creando usuario'
-    });
+    } catch (error) {
+        console.log('error :>> ', error);
+        res.status(500).json({
+            ok:false,
+            msg: 'Hable con el administrador.'
+        });
+    }
+
 }
 
 module.exports = {
