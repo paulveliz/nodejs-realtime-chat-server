632573b3bc997eb4d16c6139183c2ce5d318bfe4
Paul guadalupe veliz lopez <paulsystem32@gmail.com>
2020-12-09 12:53:03 -0700

    Middleware personalizado
    - Creamos nuestro propio mw para validar los campos.

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,14 +1,6 @@
 const { response } = require('express');
-const { validationResult } = require('express-validator');
 
 const crearUsuario = (req, res = response) =>{
-    const errores = validationResult(req);
-    if(!errores.isEmpty()){
-        return res.status(400).json({
-            ok: false,
-            errors: errores.mapped()
-        });
-    }
     res.json({
         ok: true,
         msg: 'Creando usuario'
--- routes/auth.js
+++ routes/auth.js
@@ -4,10 +4,13 @@
 const { Router } = require('express');
 const { check } = require('express-validator');
 const { crearUsuario } = require('../controllers/auth');
+const { validarCampos } = require('../middlewares/validar-campos');
 const router = Router();
 
 router.post('/new', [
-    check('nombre', 'El nombre es obligatorio.').not().isEmpty()
+    check('nombre', 'El nombre es obligatorio.').not().isEmpty(),
+    check('password', 'La contraseña es obligatorio.').isEmail(),
+    validarCampos
 ], crearUsuario);
 
 module.exports = router;
