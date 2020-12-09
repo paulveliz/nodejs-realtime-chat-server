62dbcc0ad6a6789cb976e3648f46aa5b873fdf47
Paul guadalupe veliz lopez <paulsystem32@gmail.com>
2020-12-09 01:04:53 -0700

    Conexion con db
    - Nos conectamos con atlas.

--- database/config.js
+++ database/config.js
@@ -2,7 +2,12 @@ const mongoose = require('mongoose');
 
 const dbConnection = async () => {
     try {
-        console.log('Init db config');
+        await mongoose.connect(process.env.DB_CNN, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            useCreateIndex: true
+        });
+        console.log('Db online');
     } catch (error) {
        console.log(error); 
        throw new Error('Error detectado');
