92dd02e7f9282216aaa5d402bd1763a5eabc6d7e
Paul guadalupe veliz lopez <paulsystem32@gmail.com>
2020-12-09 12:57:40 -0700

    Creando modelo usuario con Schema mongoose
    Creamos el modelo de usuario utilizando el orm mongoose.

---
+++ models/usuario.js
@@ -0,0 +1,25 @@
+const  { Schema, model } = require('mongoose');
+
+const UsuarioSchema = Schema({
+
+    nombre: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    online: {
+        type: Boolean,
+        default: false,
+    },
+
+});
+
+module.exports = model('Usuario', UsuarioSchema);
