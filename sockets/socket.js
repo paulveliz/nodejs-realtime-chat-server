0123ecae529e60fa306ca9dd695ae898cf080593
Paul guadalupe veliz lopez <paulsystem32@gmail.com>
2020-12-17 17:31:35 -0700

    Actualizad db al conectar/desconectar
    En base al token y el uid se actualizara la base de datos indicando si
    el usuario esta conectado o no.

--- sockets/socket.js
+++ sockets/socket.js
@@ -1,15 +1,21 @@
 const { comprobarJWT } = require('../helpers/jwt');
 const { io } = require('../index');
+const { usuarioConectado, usuarioDesconectado } = require('../controllers/socket');
 
 // Mensajes de sockets
 io.on('connection', client => {
+
     console.log('Cliente conectado');
     const [ valido, uid ] = comprobarJWT(client.handshake.headers['x-token']);
-    
+
+    // Verificar autenticacion.    
     if(!valido) return client.disconnect();
-    console.log('Cliente autenticado.');
+
+    // Cliente autenticado
+    usuarioConectado( uid );
 
     client.on('disconnect', () => { 
-        console.log('Cliente desconectado.');
+        usuarioDesconectado( uid );
      });
+
 });
